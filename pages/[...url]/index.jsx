e9cf346820113642eca00ed13847e4ee4367ba7e
harish <chandharish198@gmail.com>
2022-10-19 22:22:32 +0530

    image added

--- pages/[...url]/index.jsx
+++ pages/[...url]/index.jsx
@@ -1,21 +1,29 @@
+import Head from 'next/head';
 import React, { useEffect } from 'react'
 
 export default function Ix() {
 
+    let mainurl = '';
     useEffect(() => {
         let data = location.href.split('/').slice(3);
-        let mainurl = '';
 
         data.map(x => {
             mainurl += '/' + x
         })
 
+
         console.log(mainurl)
-        location.href = 'https://dailypositive24.com'+mainurl
+        location.href = 'https://dailypositive24.com' + mainurl
     }, [])
 
 
     return (
-        <div></div>
+        <div>
+
+            <Head>
+                <title></title>
+                <meta property="og:image" content={mainurl} />
+            </Head>
+        </div>
     )
 }
